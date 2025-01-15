6a5ba6023d679bdb17224bec681ff8d50ff337f0
AyakaHamasaki <kjsycz@gmail.com>
2025-01-15 05:24:34 +0000

    kadai8

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
+
+Route::controller(AAAController::class)->group(function() {
+    Route::get('XXX/news/create', 'bbb');
+});
+
+
+{
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+public function add()
+{
+    return view('admin.profile.create');
+}
+
+public function create()
+{
+    return redirect('admin/profile/create');
+}
+
+public function edit()
+{
+    return view('admin.profile.edit');
+}
+
+public function update()
+{
+    return redirect('admin/profile/edit');
+}
+//
+}
